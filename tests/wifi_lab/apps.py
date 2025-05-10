7cc71bf26d28f418afe022bf77f2faa22b4a085b
PatrikMalina <patrik.malina.jr@gmail.com>
2025-05-10 13:42:33 +0200

    Add custom icons for menu

--- tests/wifi_lab/apps.py
+++ tests/wifi_lab/apps.py
@@ -9,28 +9,116 @@ def register_laboratory_menu():
         config={
             'label': _('Laboratory'),
             'name': 'laboratory',
-            'icon': 'ow-device',
+            'icon': 'icon-laboratory',
             'items': {
                 1: {
                     'label': _('Laboratory Exercises'),  # Label displayed in the menu
                     'url': '/admin/wifi_lab/labexercise/',  # URL to the admin changelist page
                     'name': 'labexercise',  # A unique identifier for this menu item
-                    'icon': 'ow-device',  # Optional icon class (from OpenWISP themes)
+                    'icon': 'icon-lab-exercise',
                 },
                 2: {
                     'label': _('Scripts'),
                     'url': '/admin/script_manager/scriptrecord/',
                     'name': 'scriptrecord',
-                    'icon': 'ow-template',
+                    'icon': 'icon-script',
                 },
                 3: {
                     'label': _('Custom Clients'),
                     'url': '/admin/device_manager/device/',
                     'name': 'customdevice',
-                    'icon': 'ow-device',
+                    'icon': 'icon-custom-device',
+                },
+                4:{
+                    'label': _('File Uploads'),
+                    'url': '/admin/file_manager/filerecord/',
+                    'name': 'filerecord',
+                    'icon': 'ow-template',
                 }
             }
         }
+    )
+
+
+def register_notification_types():
+    from device_manager.models import Device
+    from wifi_lab.models import LabExercise
+    from openwisp_notifications.types import register_notification_type
+
+    register_notification_type(
+        'custom_device_registered',
+        {
+            'verbose_name': _('Custom Device Registration'),
+            'verb': _('custom device registered successfully'),
+            'level': 'success',
+            'email_subject': _(
+                '[{site.name}] SUCCESS: "{notification.target}"'
+                ' {notification.verb}'
+            ),
+            'email_notification': False,
+            'message': _(
+                'A new custom device [{notification.target}]'
+                '({notification.target_link}) has registered successfully.'
+            ),
+        },
+        models=[Device],
+    )
+
+    register_notification_type(
+        'lab_exercise_started',
+        {
+            'verbose_name': _('Laboratory Exercise Started'),
+            'verb': _('laboratory exercise started successfully'),
+            'level': 'success',
+            'email_subject': _(
+                '[{site.name}] SUCCESS: "{notification.target}"'
+                ' {notification.verb}'
+            ),
+            'email_notification': False,
+            'message': _(
+                'A laboratory [{notification.target}]'
+                '({notification.target_link}) has started successfully.'
+            ),
+        },
+        models=[LabExercise],
+    )
+
+    register_notification_type(
+        'lab_exercise_stopped',
+        {
+            'verbose_name': _('Laboratory Exercise Stopped'),
+            'verb': _('laboratory exercise stopped successfully'),
+            'level': 'success',
+            'email_subject': _(
+                '[{site.name}] SUCCESS: "{notification.target}"'
+                ' {notification.verb}'
+            ),
+            'email_notification': False,
+            'message': _(
+                'A laboratory [{notification.target}]'
+                '({notification.target_link}) has stopped successfully.'
+            ),
+        },
+        models=[LabExercise],
+    )
+
+    register_notification_type(
+        'lab_exercise_error',
+        {
+            'verbose_name': _('Laboratory Exercise Error'),
+            'verb': _('laboratory exercise error'),
+            'level': 'success',
+            'email_subject': _(
+                '[{site.name}] SUCCESS: "{notification.target}"'
+                ' {notification.verb}'
+            ),
+            'email_notification': False,
+            'message': _(
+                'A laboratory [{notification.target}]'
+                '({notification.target_link}) has error on one or more devices.'
+            ),
+        },
+        models=[LabExercise],
     )
 
 
@@ -40,3 +128,6 @@ class WifiLabConfig(AppConfig):
 
     def ready(self):
         register_laboratory_menu()
+        register_notification_types()
+
+        import wifi_lab.signals # noqa: F401
